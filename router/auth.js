3bf059b610aaf0019b29135941eeac0bac712f48
Van Quang <vanquanggt01@gmail.com>
2023-11-01 20:18:24 +0700

    fix user

--- router/auth.js
+++ router/auth.js
@@ -6,7 +6,7 @@ const router = require('express').Router()
 router.post('/register', AuthController.register)
 router.post('/login', AuthController.login)
 router.post('/refresh-token', AuthController.requestRefreshToken)
-router.post('/logout ', AuthController.logout)
+router.post('/logout', AuthController.logout)
 router.get('/forgot-password', AuthController.forgotPassword)
 router.put('/reset-password', AuthController.resetPassword)
 
