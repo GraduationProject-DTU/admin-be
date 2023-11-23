82353e5fd59a62609ffd490d0d19c7e4a9a80a7a
Van Quang <vanquanggt01@gmail.com>
2023-11-23 19:00:54 +0700

    fix router  auth

--- router/auth.js
+++ router/auth.js
@@ -7,7 +7,7 @@ router.post('/register', AuthController.register)
 router.post('/login', AuthController.login)
 router.post('/refresh-token', AuthController.requestRefreshToken)
 router.post('/logout', AuthController.logout)
-router.get('/forgot-password', AuthController.forgotPassword)
+router.post('/forgot-password', AuthController.forgotPassword)
 router.put('/reset-password', AuthController.resetPassword)
 
 module.exports = router
